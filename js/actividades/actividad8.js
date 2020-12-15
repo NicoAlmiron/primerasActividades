351fbc34e3f83b72d7fd2b7aa0c68f7b98b36558
Nico Almiron <almiron.nicolas16@gmail.com>
2020-12-15 20:45:55 -0300

    se realizo el ejercicio 8

---
+++ js/actividades/actividad8.js
@@ -0,0 +1,7 @@
+let numero1 = parseInt(prompt("Ingrese un numero"));
+
+if ((numero1 % 2) == 0) {
+    document.write("El numero ingresado SI es divisible por 2");
+} else {
+    document.write("El numero ingresado NO es divisible por 2");
+}
