4dbfd9b571870641d5a3a02fba35c44cf1a15c35
Nico Almiron <almiron.nicolas16@gmail.com>
2020-12-15 22:45:16 -0300

    se realizo el ejercicio 11

---
+++ js/actividades/actividad11.js
@@ -0,0 +1,19 @@
+let numero1 = parseInt(prompt("Ingrese un numero"));
+
+document.write("El numero ingresado es divisible por: ");
+
+if (numero1 % 2 == 0) {
+    document.write(" 2 ");
+}
+
+if (numero1 % 3 == 0) {
+    document.write(" 3 ");
+}
+
+if (numero1 % 5 == 0) {
+    document.write(" 5 ");
+}
+
+if (numero1 % 7 == 0) {
+    document.write(" 7 ");
+}
